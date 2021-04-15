1078decf32745762194dbb2b030c7e9134c19358
Héctor <hectorcrespuig@gmail.com>
2021-04-15 19:18:20 +0200

    Inicio

---
+++ src/test/java/com/proyecto/android/api/ApiProyectoAndroidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.android.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProyectoAndroidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
